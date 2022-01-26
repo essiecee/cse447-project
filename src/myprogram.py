caac87c60427267132bf43f73efd3f82ec96f7ec
davidchang <changd8@uw.edu>
2022-01-25 17:28:34 -0800

    added functions for building context and getting predictions"

--- src/myprogram.py
+++ src/myprogram.py
@@ -7,6 +7,58 @@ from datasets import load_dataset
 import nltk
 from nltk import word_tokenize
 from nltk.util import ngrams
+from nltk import FreqDist
+from typing import Dict
+from collections import defaultdict
+
+
+def build_contexts(text, context_dict: Dict[tuple, Dict[str, int]], n):
+    """
+    build n_grams given n
+    assigns each word x_t and their respective counts to their context x_{t-n+1:t-1}
+    """
+    tokens = ['<s>'] * (n - 1)
+    tokens.extend(word_tokenize(text))
+    tokens.append('</s>')
+    n_grams = ngrams(tokens, n)
+
+    for ngram in n_grams:
+        context = ngram[:-1]
+        target = ngram[-1]
+        context_dict[context][target] += 1
+
+
+def get_pred(context_input, context_dict, num_preds=3):
+    """
+    return 3 distinct word predictions given context_input
+    """
+    # we can assume there is at least 3 possible next words
+    # given context, if we use enough data
+    assert len(context_dict[context_input]) >= num_preds
+    # TODO: need to consider if context_input doesn't exist in context_dict
+
+    # sorts frequency of each word given context in descending order
+    topk_preds = context_dict[context_input].most_common()
+    pred_list = [None] * num_preds
+    # ensure distinct word predictions
+    ignore_words = set()
+
+    for i in range(num_preds):
+        prob_sum = 0
+        rand_prob = random.random()
+        for pred in topk_preds:
+            word = pred[0]
+            count = pred[1]
+            prob = count / topk_preds.N()
+            prob_sum += prob
+
+            if prob_sum > rand_prob and word not in ignore_words:
+                pred_list[i] = word
+                ignore_words.add(word)
+                break
+
+    return pred_list
+
 
 class MyModel:
     """
@@ -15,6 +67,9 @@ class MyModel:
     unigrams_count = {}
     bigrams_count = {}
     trigrams_count = {}
+    unigrams_context_freq = defaultdict(FreqDist)
+    bigrams_context_freq = defaultdict(FreqDist)
+    trigrams_context_freq = defaultdict(FreqDist)
 
     @classmethod
     def load_training_data(cls):
@@ -23,7 +78,7 @@ class MyModel:
         dataset = load_dataset('csebuetnlp/xlsum', 'english', split='train')
         sentences = []
         # TODO: set this to be dataset.num_rows when we want to train on a larger set of data
-        num_rows = 2 # dataset.num_rows
+        num_rows = 2  # dataset.num_rows
         for i in range(num_rows):
             sentences.append(dataset[i]['text'])
         # TODO: what should this function return? currently it returns a list of sentences (just two for now)
@@ -48,16 +103,23 @@ class MyModel:
     def run_train(self, data, work_dir):
         # your code here
         for paragraph in data:
-            unigrams = ngrams(paragraph.split(), 1)
+            tokens = paragraph.split()
+
+            unigrams = ngrams(tokens, 1)
             # each token in unigrams looks like ('word',)
             # consider doing extra parsing for the raw text, e.g. token[0]
             self.populate_frequency_maps(unigrams, MyModel.unigrams_count)
 
-            bigrams = ngrams(paragraph.split(), 2)
+            bigrams = ngrams(tokens, 2)
             self.populate_frequency_maps(bigrams, MyModel.bigrams_count)
 
-            trigrams = ngrams(paragraph.split(), 3)
+            trigrams = ngrams(tokens, 3)
             self.populate_frequency_maps(trigrams, MyModel.trigrams_count)
+
+            # tokenize text and build contexts for n_gram models.
+            build_contexts(paragraph, self.unigrams_context_freq, n=1)
+            build_contexts(paragraph, self.bigrams_context_freq, n=2)
+            build_contexts(paragraph, self.trigrams_context_freq, n=3)
 
     def populate_frequency_maps(self, ngrams, map):
         for token in ngrams:
@@ -68,13 +130,32 @@ class MyModel:
 
     def run_pred(self, data):
         # your code here
-        preds = []
-        all_chars = string.ascii_letters
-        for inp in data:
-            # this model just predicts a random character each time
-            top_guesses = [random.choice(all_chars) for _ in range(3)]
-            preds.append(''.join(top_guesses))
-        return preds
+        unigram_preds = []
+        bigram_preds = []
+        trigram_preds = []
+        for seq in data:
+            seq_list = seq.split()
+            unigram_context = ()
+            bigram_context = seq_list[-1:]
+            trigram_context = seq_list[-2:]
+
+            unigram_top_guesses = get_pred(unigram_context, self.unigrams_context_freq)
+            unigram_preds.append(''.join(unigram_top_guesses))
+            bigram_top_guesses = get_pred(bigram_context, self.bigrams_context_freq)
+            bigram_preds.append(''.join(bigram_top_guesses))
+            trigram_top_guesses = get_pred(trigram_context, self.trigrams_context_freq)
+            trigram_preds.append(''.join(trigram_top_guesses))
+
+        return unigram_preds, bigram_preds, trigram_preds
+
+        # # starter code for reference
+        # preds = []
+        # all_chars = string.ascii_letters
+        # for inp in data:
+        #     # this model just predicts a random character each time
+        #     top_guesses = [random.choice(all_chars) for _ in range(3)]
+        #     preds.append(''.join(top_guesses))
+        # return preds
 
     def save(self, work_dir):
         # your code here
@@ -109,6 +190,7 @@ if __name__ == '__main__':
         model = MyModel()
         print('Loading training data')
         train_data = MyModel.load_training_data()
+        print('train_data:', train_data)
         print('Training')
         model.run_train(train_data, args.work_dir)
         print('Saving model')
